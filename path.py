36b2208995d93a5f148ed478ad97a82da36f2a80
ForrestHurley <forrestelihurley0@gmail.com>
2018-05-01 10:14:33 -0400

    Work on infrastructure for velocity profiling, move iteration into constraint (which is basically serving as the profile)

---
+++ path.py
@@ -0,0 +1,140 @@
+import numpy as np
+from matplotlib import pyplot as plt
+from scipy import interpolate
+
+class path(object):
+    def __init__(self,points,resolution=1000):
+        self._raw_points = np.array(points)
+        self._resolution = resolution
+       
+        self._nullify_properties()
+ 
+    def _nullify_properties(self):
+        self._arc_length = None
+        self._point_interpolator = None
+        self._raw_interp_points = None
+        self._derivatives = None
+        self._uniform_points = None
+        self._curvature = None
+        self._t_hat = None
+        self._n_hat = None
+        self._t_values = None
+
+    @property
+    def raw_points(self):
+        return self._raw_points
+
+    @raw_points.setter
+    def raw_points(self,points):
+        self._nullify_properties()
+        self._raw_points = np.array(points)
+
+    @property
+    def resolution(self):
+        return self._resolution
+
+    @resolution.setter
+    def resolution(self,resolution):
+        self._nullify_properties()
+        self._resolution = resolution
+
+    @property
+    def point_interpolator(self):
+        if (self._point_interpolator is None):
+            self._point_interpolator = interpolate.make_interp_spline(
+                                            x = np.arange(self.raw_points.shape[1]),
+                                            y = self.raw_points,
+                                            axis = 1)
+        return self._point_interpolator
+    
+    @property
+    def t_values(self):
+        if (self._t_values is None):
+            self._t_values = np.linspace(0,self.raw_points.shape[1],self.resolution)
+        return self._t_values
+
+    @property
+    def raw_interp_points(self):
+        if (self._raw_interp_points is None):
+            self._raw_interp_points = self.point_interpolator(self.t_values)
+            
+        return self._raw_interp_points
+
+    @property
+    def arc_length(self):
+        if (self._arc_length is None):
+            distances = np.sum(np.diff(self.raw_interp_points,n=1,axis=-1)**2,axis=0)**0.5
+
+            self._arc_length = np.cumsum(distances)
+        
+        return self._arc_length
+
+    @property
+    def uniform_points(self):
+        if (self._uniform_points is None):
+            self._uniform_points = interpolate.make_interp_spline(self.arc_length,self.t_values) 
+        return self._uniform_points
+
+    @property
+    def curvature(self):
+        if (self._curvature is None):
+            first_deriv = self.point_interpolator.derivative(nu=1)(self.t_values)
+            second_deriv = self.point_interpolator.derivative(nu=2)(self.t_values)
+
+            numerator = first_deriv[0]*second_deriv[1] - first_deriv[1]*second_deriv[0]
+            denominator = (first_deriv[0]**2 + first_deriv[1]**2)**(3./2.)
+
+            self._curvature = interpolate.make_interp_spline(self.t_values, numerator / denominator)
+        return self._curvature
+
+    @property
+    def t_hat(self):
+        if (self._t_hat is None):
+            pass
+        return self._t_hat
+
+    @property
+    def n_hat(self):
+        if (self._n_hat is None):
+            pass
+        return self._n_hat
+
+    def iterate_path(self,forward = True):
+        return path_iterator(self,forward)
+
+    def __iter__(self):
+        return path_iterator(self)
+
+class path_iterator(object):
+    def __init__(self,path,forward = True,step = None):
+        self.resolution = path.resolution
+        self.path = path
+        self.forward = forward
+        if (step is None):
+            self.step = (forward * 2 - 1) * path.length / path.resolution
+        else:
+            self.step = step
+            forward = step > 0
+        if forward:
+            self.current = 0
+        else:
+            self.current = path.length
+
+    def __iter__(self):
+        return self
+
+    def next(self):
+        if self.forward:
+            if self.current > self.path.length:
+                raise StopIteration
+        else:
+            if self.current < 0:
+                raise StopIteration
+        self.current += self.step
+        spline_parameter = self.path.uniform_points(self.current)
+        location = self.path.raw_interpolator(spline_parameter)
+        curvature = self.path.curvature(spline_parameter)
+
+        #current arclength, distance, spline parameter, x y location, curvature from interpolator
+        return self.current, self.step, spline_parameter, location, curvature
+
--- profile_constraints.py
+++ profile_constraints.py
@@ -6,28 +6,32 @@ class constraint(object):
         self.interp_degree_list = [[]]
         self.initials = []
         self.finals = []
-        self.bounds = [-np.inf,np.inf]
+        self.bounds = [0,10]
+        self.iter_resolution = 100
 
     @classmethod
     def minimal_constraint(cls,initial_vel=0,final_vel=0,max_vel=10,bounds = (0,10)):
         minimal = cls()
         minimal.set_start_constraint(initial_vel)
         minimal.set_end_constraint(final_vel)
-        minimal.add_interp_constraint(0,max_vel,extrapolate=True)
+        minimal.add_interp_constraint(np.array([0,max_vel]),extrapolate=True)
         minimal.set_bounds(*bounds)
         return minimal
+
+    def path_2d_acceleration(cls,path,initial_vel=0,final_vel=0,max_vel=10,max_acc=10):
+        pass
 
     def set_bounds(self,min_bound = -np.inf, max_bound = np.inf):
         self.bounds = [min_bound,max_bound]
 
-    def add_interp_constraint(self,x,y,n=1,extrapolate=False,degree=3):
+    def add_interp_constraint(self, vals, n = 1, extrapolate = False):
         if extrapolate:
-            fill = (y[0],y[-1])
+            fill = (vals[1,0],y[1,-1])
         else:
             fill = np.inf
 
         new_interp = interpolate.interp1d(
-                        x = x, y = y,
+                        x = vals[1], y = vals[0],
                         kind = 'cubic', bounds_error = False,
                         fill_value = fill)
 
@@ -35,28 +39,34 @@ class constraint(object):
             self.interp_degree_list += [[]*(n-len(self.interp_degree_list))]
 
         self.interp_degree_list[n-1].append(new_interp)
+    
+    def overwrite_interp_constraint(self, vals, n = 1, extrapolate = False):
+        pass
 
-    def set_end_constraint(self,y,n=1):
+    def overwrite_lowest_constraints(self, vals, extrapolate = False):
+        pass
+
+    def set_start_constraint(self,y,n=1):
         if n > len(self.initials):
-            self.initials += [0.]*(n-len(self.interp_degree_list))
+            self.initials += [0.]*(n-len(self.initials))
        
         self.initials[n-1] = y 
 
-    def set_start_constraint(self, y, n = 1):
+    def set_end_constraint(self, y, n = 1):
         if n > len(self.finals):
-            self.finals += [0.]*(n-len(self.interp_degree_list))
+            self.finals += [0.]*(n-len(self.finals))
        
         self.finals[n-1] = y 
 
-    def get_mins(self,points):
-        mins = np.zeros(len(self.interp_degree_list),points.shape[0])
+    def get_maxs(self,points):
+        maxs = np.zeros(len(self.interp_degree_list),points.shape[0])
 
         for i in range(len(self.interp_degree_list)):
-            mins[i] = self.get_min_nth(points,n = i + 1)
+            maxs[i] = self.get_max_nth(points,n = i + 1)
 
-        return mins
+        return maxs
 
-    def get_min_nth(self,points,n=1):
+    def get_max_nth(self,points,n=1):
         degree = self.interp_degree_list[n-1]
         tmp_vals = np.zeros(len(degree),points.shape[0])
 
@@ -72,6 +82,28 @@ class constraint(object):
 
         return raw_min
 
-    def get_min_velocity(self,points):
-        return self.get_min_nth(points,n=1)
+    def get_max_velocity(self,points):
+        return self.get_max_nth(points,n=1)
 
+    def __iter__(self):
+        return get_as_array(self)
+
+    def get_as_array(self,point_count = None):
+        if point_count is None:
+            point_count = self.iter_resolution
+        points = np.linspace(self.bounds[0],self.bounds[1],point_count)
+        return self.get_maxs(points)
+
+    def apply_iterative_func(self,foo,point_count = None,forward = True, history_size = 1):
+        if point_count is None:
+            point_count = self.iter_resolution
+
+        val_array = self.get_as_array(point_count)
+        if forward:
+            val_array = np.fliplr(val_array)
+
+        new_vals = np.full(val_array.shape,np.inf)
+        for i in range(history_size, val_array):
+            pass
+       
+        self.overwrite_lowest_constraints(new_vals, extrapolate = True) 
